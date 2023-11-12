b3c849b53293bc4b84dfc8311d1e2cf092be70e2
Jan Vlasatý <jan.vlasaty@melowntech.com>
2023-11-12 17:54:29 +0100

    Fix directions in Chapters

--- src/app/data/chapters.ts
+++ src/app/data/chapters.ts
@@ -4,7 +4,7 @@ import { TrackId } from "./tracks";
 export const Chapters: Chapter[] = [
   {
     title: "Co se stalo?",
-    directions: "Jděte po nábřeží, dále kolem divadla Na Zábradlí až před Divadelní fakultu AMU",
+    directions: "Jděte po nábřeží, kolem divadla Na Zábradlí až před Divadelní fakultu AMU",
     durationMinutes: 14,
     description: "",
     pathIndex: 0,
@@ -12,7 +12,7 @@ export const Chapters: Chapter[] = [
   },
   {
     title: "Střetnutí s letáky a prádlem",
-    directions: "Vydejte se k&nbsp;mléčnému baru (dnešní pivnice U&nbsp;Kata) přes Mariánské náměstí, ulici dál za&nbsp;magistrátem.",
+    directions: "Vydejte se k&nbsp;mléčnému baru (dnes pivnice U&nbsp;Kata) přes Mariánské náměstí, za&nbsp;magistrát HMP.",
     durationMinutes: 3,
     description: "",
     pathIndex: 1,
@@ -20,7 +20,7 @@ export const Chapters: Chapter[] = [
   },
   {
     title: "Ty jsi tam byl taky?",
-    directions: "Jděte směrem k&nbsp;Národní třídě, těsně před ní zahněte do&nbsp;ulice Bartolomějská k&nbsp;Hlavnímu sídlu StB.",
+    directions: "Jděte směrem k&nbsp;Národní třídě, k&nbsp;Hlavnímu sídlu StB na začátku ulice Bartolomějská.",
     durationMinutes: 10,
     description: "",
     pathIndex: 2,
@@ -28,7 +28,7 @@ export const Chapters: Chapter[] = [
   },
   {
     title: "Citlivé materiály",
-    directions: "Pokračujte dál směrem k&nbsp;Vltavě, Až&nbsp;dojděte na&nbsp;křižovatku s&nbsp;divadelní ulicí dejte se doleva podchodem, pokračujte dále až za&nbsp;národní divadlo do&nbsp;ulice na&nbsp;struze k&nbsp;archivu StB (dnes Archiv bezpečnostních složek).",
+    directions: "Pokračujte Bartolomějskou k&nbsp;Vltavě, poté doleva v Divadelní ulicí podchodem až do&nbsp;ulice Na&nbsp;Struze k&nbsp;archivu StB (dnes Archiv bezpečnostních složek).",
     durationMinutes: 7,
     description: "",
     pathIndex: 3,
@@ -36,7 +36,7 @@ export const Chapters: Chapter[] = [
   },
   {
     title: "Ve škole života není prázdnin",
-    directions: "Vydejte se směrem k Národní třídě a pozorujte mapu. Budete procházet pasáží mezi domy s kavárnou ve vnitro bloku, nadruhé straně je tramvajová zastávka Lazarská. Od ní pokračujete k bráně před budovou Pedagogické fakulty Karlovy Univerzity. Kdyby byl náhodou průchod zavřený, tak blok domů obejděte a vraťte se na cestu na Lazarské",
+    directions: "Vydejte se směrem ke stanici Národní třída. Pokud nebude zavřený, použijte průchod na&nbsp;zastávku Lazarská až k pedagogické fakultě UK.",
     durationMinutes: 9,
     description: "",
     pathIndex: 4,
@@ -44,7 +44,7 @@ export const Chapters: Chapter[] = [
   },
   {
     title: "Divadelní stávka",
-    directions: "Pokračujte dál podle mapy, po průchodu pasáží dojděte před Činoherní Klub",
+    directions: "Pokračujte Vodičkovou, doprava do ulice V&nbsp;Jámě a pak Štěpánskou pasáží přejděte před Činoherní Klub",
     durationMinutes: 12,
     description: "",
     pathIndex: 5,
@@ -52,7 +52,7 @@ export const Chapters: Chapter[] = [
   },
   {
     title: "Co bude pak?",
-    directions: "Jděte na Václavské náměstí a pokračujte směrem dolů až k tramvají a lavičkám s výhledem na balkón paláce Hvězda, sídla nakladatelství Melantrich (dnes je na domě nápis Marks & Spencer)",
+    directions: "Jděte na Václavské náměstí dolu k&nbsp;tramvajím před balkon paláce Hvězda (dříve Melantrich, dnes M&amp;S)",
     durationMinutes: 6,
     description: "",
     pathIndex: 6,
