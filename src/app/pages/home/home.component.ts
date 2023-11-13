aec7d0f8878f796a1ae2f6d6c6e35e8a7e2591c1
Martin Kopeček <martin.kopecek@smallhill.cz>
2023-11-13 00:35:14 +0000

    fix: typo in dikyzemuzem project name

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,9 +1,9 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  selector: "app-home",
+  templateUrl: "./home.component.html",
+  styleUrls: ["./home.component.scss"],
 })
 export class HomeComponent {
   features = [
@@ -18,15 +18,14 @@ export class HomeComponent {
       text: "Audio si můžete na webu stáhnout předem offline. Objem dat je cca 70MB.",
     },
     {
-      title: "Díky že můžem",
+      title: "Díky, že můžem",
       icon: "heart",
-      text: "Projekt byl vytvořen pod záštitou projektu Díky že můžem",
+      text: "Projekt byl vytvořen pod záštitou projektu Díky, že můžem, z. s.",
     },
     {
       title: "DAMU",
       icon: "heart",
       text: "Audiowalk byl natočen díky umělcům z Divadelní fakulty AMU",
-    }
+    },
   ];
-
 }
